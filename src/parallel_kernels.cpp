6ad28f9315cc9fdbcbec6cd125287613205d3a05
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-12-13 17:59:05 +0100

    Changed array to vector

--- src/parallel_kernels.cpp
+++ src/parallel_kernels.cpp
@@ -1,6 +1,7 @@
 #include <cuda_profiler_api.h>
 
 #include <memory>
+#include <vector>
 
 #define stringify(x) #x
 #ifdef _OPENMP
@@ -14,7 +15,7 @@
 #endif
 
 template<typename T>
-void init(int* data, std::size_t data_size, std::array<T,2> streams, int queue = 0) {
+void init(int* data, std::size_t data_size, std::vector<T> streams, int queue = 0) {
   acc_pragma(parallel loop present(data[0:data_size]))
   omp_pragma(target teams distribute parallel for simd depend(out: streams[queue]) nowait)
   for(auto i = 0; i < data_size; ++i) {
@@ -23,7 +24,7 @@ void init(int* data, std::size_t data_size, std::array<T,2> streams, int queue =
 }
 
 template<typename T>
-void compute(int* data, std::size_t data_start, std::size_t data_size, std::array<T,2> streams, int queue) {
+void compute(int* data, std::size_t data_start, std::size_t data_size, std::vector<T> streams, int queue) {
 //void compute(int* data, std::size_t data_start, std::size_t data_size, T* streams, int queue) {
   acc_pragma(parallel loop present(data[data_start:data_size]) async(streams[queue]))
   omp_pragma(target teams distribute parallel for simd depend(inout: streams[queue]) nowait)
@@ -42,7 +43,9 @@ int main() {
   // streams being a vector is not working
   // auto const nstreams = 2;
   // auto* streams = new int[nstreams];
-  std::array<int,2> streams;
+  auto const nstreams = 2;
+  std::vector<int> streams;
+  streams.reserve(nstreams);
   streams[0] = 0;
   streams[1] = 1;
   acc_pragma(data create(data[0:data_size]))
